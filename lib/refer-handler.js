havfo/drachtio-fn-b2b-sugar

commit a5cdf2c586d198c5cafe9f3628d6623fba4afe69

added better errors and fixed referredBy header parsing

lib/refer-handler.js
@@ -1,4 +1,4 @@
-const debug = require('debug')
+const debug = require('debug');
 const parseUri = require('drachtio-srf').parseUri;
 const assert = require('assert');
 
@@ -24,7 +24,7 @@ class ReferHandler {
 
     this.req = opts.req;
     this.res = opts.res;
-    this.srf = opts.req.srf;
+    this.srf = opts.srf;
     this.transferorDialog = opts.transferor;
     this.transfereeDialog = opts.transferor.other;
     if (opts.authLookup) {
@@ -37,7 +37,7 @@ class ReferHandler {
 
   // some phones will send a REFER-TO uri without any '@' symbol, just <sip:500>, 
   // so I slightly modified the drachtio-sip uri parser here to catch it
-  shortReferToUriParser(s) {
+  _shortReferToUriParser(s) {
     if (typeof s === 'object')
       return s;
 
@@ -63,7 +63,7 @@ class ReferHandler {
     }
   }
 
-  referToUriParser(referToUri) {
+  _referToUriParser(referToUri) {
     const referUri = parseUri(referToUri) ? parseUri(referToUri) : this._shortReferToUriParser(referToUri);
     const referParams = referUri;
 
@@ -139,14 +139,14 @@ class ReferHandler {
           debug(`ReferHandler#_handleRefer No destination IP was provided or \
             could be found for ${referParams.user}`);
           this.res.send(405);
-          return;
+          reject();
         }
       } else {
         if (referParams.destinationIp === undefined) {
           debug(`ReferHandler#_handleRefer No destination IP was provided or \
             could be found for ${referParams.user}`);
           this.res.send(405);
-          return;
+          reject();
         }
         transferTargetDestination = referParams.destinationIp;
       }
@@ -193,7 +193,8 @@ class ReferHandler {
       // find transferTarget Dialog
       const replacesDialog = this.srf.findDialogByCallIDAndFromTag(referParams.replacesCallId,
         referParams.replacesFromTag);
-      debug(replacesDialog);
+      debug('ReferHandler#_handleReplacesRefer found replacesDialog. \
+            Calling replacesDialog.other to get the side of the call to transfer to.');
       const transferTargetDialog = replacesDialog.other;
 
       // send NOTIFY 100 Trying
@@ -223,47 +224,45 @@ class ReferHandler {
       const referTo = this.req.getParsedHeader('Refer-to');
       const referParams = this.referToUriParser(referTo.uri);
 
-      referParams.referredBy = this.req.getParsedHeader('Referred-By'); // need to parse for the to user
+      const referredByUri = this.req.getParsedHeader('Referred-By').replace('<', '').replace('>', '');
+      referParams.referredBy = this._referToUriParser(referredByUri);
 
       if (this.authLookup) {
-        if (!this.authLookup(referParams)) {
+        if (!this.authLookup(referParams.referredBy.user)) {
           this.res.send(403);
-          return;
+          reject(`ReferHandler# error transfering call. ${referParams.referredBy.user} not authorized`);
         }
       }
       this.res.send(202);
 
       this.transferorDialog.on('destroy', () => {
-        debug('ReferHandler# transferor dialog destroyed.');
+        debug('ReferHandler# transferor hungup.');
       });
 
       try {
         if (referParams.replaces) {
           const transferTargetDialog = await this._handleReplacesRefer(referParams);
-
           // Link hangup between the two dialogs
-          [transferTargetDialog, this.transfereeDialog].forEach((dialog) => {
-            dialog.on('destroy', () => {
-              debug('ReferHandler#_handleReplacesRefer call ended');
-              (dialog === transferTargetDialog ? this.transfereeDialog : transferTargetDialog).destroy();
-            });
-          });
-
-          resolve({ transfereeDialog: this.transfereeDialog, transferTargetDialog});
-        } else {
-          const transferTargetDialog = await this._handleReplacesRefer(referParams);
-          // Link hangup between the two dialogs
-          [transferTargetDialog, this.transfereeDialog].forEach((dialog) => {
+          [this.transfereeDialog, transferTargetDialog].forEach((dialog) => {
             dialog.on('destroy', () => {
               debug('call ended');
               (dialog === transferTargetDialog ? this.transfereeDialog : transferTargetDialog).destroy();
             });
           });
-
+          resolve({ transfereeDialog: this.transfereeDialog, transferTargetDialog});
+        } else {
+          const transferTargetDialog = await this._handleRefer(referParams);
+          // Link hangup between the two dialogs
+          [this.transfereeDialog, transferTargetDialog].forEach((dialog) => {
+            dialog.on('destroy', () => {
+              debug('call ended');
+              (dialog === transferTargetDialog ? this.transfereeDialog : transferTargetDialog).destroy();
+            });
+          });
           resolve({ transfereeDialog: this.transfereeDialog, transferTargetDialog});
         }
       } catch (error) {
-        reject(`ReferHandler# error transfering call to ${referParams.user}`);
+        reject(`ReferHandler# error transfering call: ${error}`);
       }
     });
   }

commit 270253b95fbbbc51ed51416702531ac5c4e3eba1

Wrong function reference
Fix for regression in commit a5cdf2c586d198c5cafe9f3628d6623fba4afe69

lib/refer-handler.js
@@ -222,7 +222,7 @@ class ReferHandler {
   transfer() {
     return new Promise(async(resolve, reject) => {
       const referTo = this.req.getParsedHeader('Refer-to');
-      const referParams = this.referToUriParser(referTo.uri);
+      const referParams = this._referToUriParser(referTo.uri);
 
       const referredByUri = this.req.getParsedHeader('Referred-By').replace('<', '').replace('>', '');
       referParams.referredBy = this._referToUriParser(referredByUri);